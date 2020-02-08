77248ce1e620bcdbd0f905b28fc668affa8af9fe
Yang Luo <hsluoyz@qq.com>
2020-02-08 16:25:49 +0800

    Generate embed code from config.

--- web/src/Config.js
+++ web/src/Config.js
@@ -16,7 +16,58 @@ class Config extends React.Component {
     };
   }
 
-  render() {
+  setHeight(codeLinesNumber) {
+    if (this.instance === undefined) {
+      return;
+    }
+
+    // [question] Is there a way to set height of component by a number of lines?
+    // https://github.com/scniro/react-codemirror2/issues/124#issuecomment-460038374
+    const height = `${codeLinesNumber * 25}px`;
+    this.instance.setSize('100%', height);
+  }
+
+  getConfigText(website) {
+    let res = "let config = {";
+
+    if (website.trackConfig.uploadMode !== "periodic") {
+      res += `\n      uploadMode: "${website.trackConfig.uploadMode}",`;
+
+      if (website.trackConfig.frequency !== 50) {
+        res += `\n      frequency: ${website.trackConfig.frequency},`;
+      }
+    } else {
+      if (website.trackConfig.uploadPeriod !== 5000) {
+        res += `\n      uploadPeriod: ${website.trackConfig.uploadPeriod},`;
+      }
+    }
+
+    if (website.trackConfig.encoder !== "JSON.stringify") {
+      res += `\n      encoder: "${website.trackConfig.encoder}",`;
+    }
+
+    if (website.trackConfig.decoder !== "x => x") {
+      res += `\n      decoder: "${website.trackConfig.decoder}",`;
+    }
+
+    if (website.trackConfig.enableGet !== false) {
+      res += `\n      enableGet: ${website.trackConfig.enableGet},`;
+    }
+
+    if (website.trackConfig.resendInterval !== 3000) {
+      res += `\n      resendInterval: ${website.trackConfig.resendInterval},`;
+    }
+
+    if (res === "let config = {") {
+      return "";
+    }
+
+    res += `\n    };\n\n    `;
+
+    return res;
+  }
+
+  getCode() {
     const version = "latest";
     const website = this.props.website;
 
@@ -25,7 +76,7 @@ class Config extends React.Component {
   var script = document.createElement("script");
   script.src = "https://cdn.jsdelivr.net/npm/mouselog@${version}/mouselog.js";
   script.onload = () => {
-    mouselog.run("${website.trackConfig.uploadEndpoint}", "${website.id}");
+    ${this.getConfigText(website)}mouselog.run("${website.trackConfig.uploadEndpoint}", "${website.id}");
   };
   var t = document.getElementsByTagName("script");
   var s = t.length > 0 ? t[0].parentNode : document.body;
@@ -33,11 +84,24 @@ class Config extends React.Component {
 })();
 </script>`;
 
+    const lineCount = code.split(`\n`).length - 1;
+    this.setHeight(lineCount);
+
+    return code;
+  }
+
+  render() {
+    const code = this.getCode();
+
     // How do I crop the contents of an Iframe to show a part of a page?
     // https://stackoverflow.com/questions/5676672/how-do-i-crop-the-contents-of-an-iframe-to-show-a-part-of-a-page
     return (
-      <div>
+      <div style={{width: "650px"}}>
         <CodeMirror
+          editorDidMount={editor => {
+            this.instance = editor;
+            this.getCode();
+          }}
           value={code}
           options={{mode: 'javascript', theme: "material-darker"}}
         />
