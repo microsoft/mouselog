67d88b3ae19540ad93bfdedea3ae7555fc33fa15
Yang Luo <hsluoyz@qq.com>
2020-02-12 19:01:56 +0800

    Fix bottom footer.

--- web/src/App.js
+++ web/src/App.js
@@ -95,118 +95,121 @@ class App extends React.Component {
            onTouchMove={(e) => Setting.mouseHandler('touchmove', e)}
            onTouchEnd={(e) => Setting.mouseHandler('touchend', e)}
       >
-        <Layout className="layout">
-          <Header style={{padding: '0', marginBottom: '3px'}}>
-            <div className="logo"/>
+        <div id="content-wrap">
+          <Layout className="layout">
+            <Header style={{padding: '0', marginBottom: '3px'}}>
+              <div className="logo"/>
 
-            <Menu
-              // theme="dark"
-              mode="horizontal"
-              defaultSelectedKeys={[`${this.state.selectedMenuKey}`]}
-              style={{lineHeight: '64px'}}
-              inlineCollapsed={false}
-            >
-              <Text>Mouselog</Text>
+              <Menu
+                // theme="dark"
+                mode="horizontal"
+                defaultSelectedKeys={[`${this.state.selectedMenuKey}`]}
+                style={{lineHeight: '64px'}}
+                inlineCollapsed={false}
+              >
+                <Text>Mouselog</Text>
 
-              <Menu.Item key="1">
-                <a href="/">
-                  Get Started
-                </a>
-              </Menu.Item>
-              <Menu.Item key="6">
-                <a href="/websites">
-                  Websites
-                </a>
-              </Menu.Item>
-              {
-                !this.getUrlPath().includes('sessions') ? null :
-                  <Menu.Item key="7">
-                    <a href="#">
-                      Sessions
-                    </a>
-                  </Menu.Item>
-              }
-              {
-                !this.getUrlPath().includes('impressions') ? null :
-                  <Menu.Item key="8">
-                    <a href="#">
-                      Impressions
-                    </a>
-                  </Menu.Item>
-              }
-              <Menu.Item key="2">
-                <a href="/dashboard">
-                  Dataset
-                </a>
-              </Menu.Item>
-              {
-                !this.getUrlPath().includes('trace') ? null :
-                  <Menu.Item key="3">
-                    <a href="#">
-                      Trace
-                    </a>
-                  </Menu.Item>
-              }
-              {
-                !this.getUrlPath().includes('canvas') ? null :
-                  <Menu.Item key="4">
-                    <a href="#">
-                      Canvas
-                    </a>
-                  </Menu.Item>
-              }
-              <Menu.Item key="5">
-                <a href="/rule">
-                  Rule
-                </a>
-              </Menu.Item>
-              <Menu.Item key='5' style={{float: 'right'}}>
-                <a target="_blank" href="https://github.com/microsoft/mouselog">
-                  <img alt="GitHub stars" src="https://img.shields.io/github/stars/microsoft/mouselog?style=social" />
-                </a>
-              </Menu.Item>
-              {
-                this.getUrlPath() !== "/" ? null :
-                  <Text style={{float: 'right'}}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Impression ID: &nbsp;
-                    {<Tag color="#108ee9">{Setting.getImpressionId()}</Tag>}
-                  </Text>
-              }
-              {
-                this.getUrlPath() !== "/" ? null :
-                  <Text style={{float: 'right'}}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Session ID: &nbsp;
-                    {<Tag color="#108ee9">{this.state.sessionId !== '' ? this.state.sessionId : 'NULL'}</Tag>}
-                  </Text>
-              }
-              <Text style={{float: 'right'}}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Server Status: &nbsp;
-                {this.state.status ? <Tag color="#87d068">On</Tag> : <Tag color="#f50">Off</Tag>}
-              </Text>
-              {/*<Text style={{float: 'right'}}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Player Fast Forward: &nbsp;*/}
-              {/*  <AntdSwitch checked={this.state.enablePlayerFastForward} onChange={this.onSwitchChange.bind(this)}/>*/}
-              {/*</Text>*/}
-            </Menu>
-          </Header>
-        </Layout>
-
-        <Switch>
-          <Route exact path="/" component={TestPage}/>
-          <Route path="/dashboard/" component={DashboardPage}/>
-          <Route path="/trace/:sessionId" component={TracePage}/>
-          <Route path="/canvas/:sessionId/:traceId" component={CanvasPage}/>
-          <Route path="/rule/" component={RulePage}/>
-          <Route exact path="/websites/" component={WebsitePage}/>
-          <Route exact path="/websites/:websiteId" component={WebsiteEditPage}/>
-          <Route exact path="/websites/:websiteId/sessions" component={SessionPage}/>
-          <Route exact path="/websites/:websiteId/sessions/:sessionId/impressions" component={ImpressionPage}/>
-          <Route exact path="/websites/:websiteId/sessions/:sessionId/impressions/:impressionId/events" component={CanvasPage}/>
-        </Switch>
-        <Footer style={
+                <Menu.Item key="1">
+                  <a href="/">
+                    Get Started
+                  </a>
+                </Menu.Item>
+                <Menu.Item key="6">
+                  <a href="/websites">
+                    Websites
+                  </a>
+                </Menu.Item>
+                {
+                  !this.getUrlPath().includes('sessions') ? null :
+                    <Menu.Item key="7">
+                      <a href="#">
+                        Sessions
+                      </a>
+                    </Menu.Item>
+                }
+                {
+                  !this.getUrlPath().includes('impressions') ? null :
+                    <Menu.Item key="8">
+                      <a href="#">
+                        Impressions
+                      </a>
+                    </Menu.Item>
+                }
+                <Menu.Item key="2">
+                  <a href="/dashboard">
+                    Dataset
+                  </a>
+                </Menu.Item>
+                {
+                  !this.getUrlPath().includes('trace') ? null :
+                    <Menu.Item key="3">
+                      <a href="#">
+                        Trace
+                      </a>
+                    </Menu.Item>
+                }
+                {
+                  !this.getUrlPath().includes('canvas') ? null :
+                    <Menu.Item key="4">
+                      <a href="#">
+                        Canvas
+                      </a>
+                    </Menu.Item>
+                }
+                <Menu.Item key="5">
+                  <a href="/rule">
+                    Rule
+                  </a>
+                </Menu.Item>
+                <Menu.Item key='5' style={{float: 'right'}}>
+                  <a target="_blank" href="https://github.com/microsoft/mouselog">
+                    <img alt="GitHub stars" src="https://img.shields.io/github/stars/microsoft/mouselog?style=social" />
+                  </a>
+                </Menu.Item>
+                {
+                  this.getUrlPath() !== "/" ? null :
+                    <Text style={{float: 'right'}}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Impression ID: &nbsp;
+                      {<Tag color="#108ee9">{Setting.getImpressionId()}</Tag>}
+                    </Text>
+                }
+                {
+                  this.getUrlPath() !== "/" ? null :
+                    <Text style={{float: 'right'}}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Session ID: &nbsp;
+                      {<Tag color="#108ee9">{this.state.sessionId !== '' ? this.state.sessionId : 'NULL'}</Tag>}
+                    </Text>
+                }
+                <Text style={{float: 'right'}}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Server Status: &nbsp;
+                  {this.state.status ? <Tag color="#87d068">On</Tag> : <Tag color="#f50">Off</Tag>}
+                </Text>
+                {/*<Text style={{float: 'right'}}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Player Fast Forward: &nbsp;*/}
+                {/*  <AntdSwitch checked={this.state.enablePlayerFastForward} onChange={this.onSwitchChange.bind(this)}/>*/}
+                {/*</Text>*/}
+              </Menu>
+            </Header>
+          </Layout>
+          <Switch>
+            <Route exact path="/" component={TestPage}/>
+            <Route path="/dashboard/" component={DashboardPage}/>
+            <Route path="/trace/:sessionId" component={TracePage}/>
+            <Route path="/canvas/:sessionId/:traceId" component={CanvasPage}/>
+            <Route path="/rule/" component={RulePage}/>
+            <Route exact path="/websites/" component={WebsitePage}/>
+            <Route exact path="/websites/:websiteId" component={WebsiteEditPage}/>
+            <Route exact path="/websites/:websiteId/sessions" component={SessionPage}/>
+            <Route exact path="/websites/:websiteId/sessions/:sessionId/impressions" component={ImpressionPage}/>
+            <Route exact path="/websites/:websiteId/sessions/:sessionId/impressions/:impressionId/events" component={CanvasPage}/>
+          </Switch>
+        </div>
+        {/*How to keep your footer where it belongs ?*/}
+        {/*https://www.freecodecamp.org/news/how-to-keep-your-footer-where-it-belongs-59c6aa05c59c/*/}
+        <Footer id="footer" style={
           {
             borderTop: '1px solid #e8e8e8',
             backgroundColor: 'white',
             textAlign: 'center',
           }
         }>
-          Made with <span style={{color: 'rgb(255, 255, 255)'}}>❤️</span> by <a style={{fontWeight: "bold", color: "black"}} target="_blank" href="https://github.com/microsoft/mouselog">Microsoft</a>
+          Made with <span style={{color: 'rgb(255, 255, 255)'}}>❤️</span> by <a style={{fontWeight: "bold", color: "black"}} target="_blank" href="https://github.com/microsoft/mouselog">Mouselog</a>
         </Footer>
       </div>
     );
