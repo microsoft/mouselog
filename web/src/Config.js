ae1ae33d493f69c20c97aa5fe5de99962c80ccd8
kingiw <kingiw@hotmail.com>
2020-02-14 20:47:36 +0800

    remove dubugging output

--- web/src/Config.js
+++ web/src/Config.js
@@ -68,7 +68,6 @@ class Config extends React.Component {
     
     let trimmedScope = this.trimStr(website.trackConfig.scope)
     if (trimmedScope !== "window.document") {
-      console.log(website.trackConfig);
       let lines = trimmedScope.split('\n');
       res += `\n      scope: ${lines[0]}`;
       for (let i = 1; i < lines.length; ++i) {
