05befadb7f9f45f8d8bc7ff299e6bbb70cf3cc8f
kingiw <kingiw@hotmail.com>
2020-02-15 15:35:22 +0800

    fix error in code template

--- web/src/Config.js
+++ web/src/Config.js
@@ -97,7 +97,7 @@ class Config extends React.Component {
   var script = document.createElement("script");
   script.src = "https://cdn.jsdelivr.net/npm/mouselog@${version}/build/mouselog.min.js";
   script.onload = () => {
-    ${configText}var agent = mouselog.Mouselog();
+    ${configText}var agent = new mouselog.Mouselog();
     agent.run(config);
   };
   var t = document.getElementsByTagName("script");
